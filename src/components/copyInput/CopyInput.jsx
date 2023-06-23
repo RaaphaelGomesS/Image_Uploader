2ec67bebdfe1b35c2b2bc4070d3bd59c4586ad6b
Raphael Gomes <rafagomes1706@gmail.com>
2023-06-23 17:31:53 -0300

    feat: cria o input para o CardRes

---
+++ src/components/copyInput/CopyInput.jsx
@@ -0,0 +1,10 @@
+import "./copyInput.css";
+
+export default function CopyInput() {
+  return (
+    <div className="input">
+      <input></input>
+      <button>Copy Link</button>
+    </div>
+  );
+}
