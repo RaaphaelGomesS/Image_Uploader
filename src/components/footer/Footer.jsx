e4f3e15938f423571cc3984bd8ef8d05b976d0e2
Raphael Gomes <rafagomes1706@gmail.com>
2023-06-23 17:32:24 -0300

    feat: cria o component Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+
+export default function Footer() {
+  return (
+    <footer>
+      created by <a href="https://github.com/RaaphaelGomesS">RaaphaelGomesS</a>{" "}
+      - devChallengers.io
+    </footer>
+  );
+}
