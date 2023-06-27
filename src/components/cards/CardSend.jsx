2acb1aaccb15664c113771e3b1a87f43bcab55d4
Raphael Gomes <rafagomes1706@gmail.com>
2023-06-27 20:42:37 -0300

    feat: adiciona o InputImg

--- src/components/cards/CardSend.jsx
+++ src/components/cards/CardSend.jsx
@@ -1,11 +1,14 @@
 import "./card.css";
+import InputImg from "../inputImg/InputImg";
 
 export default function CardSend(props) {
   return (
     <div className="card">
       <h1>Upload your image</h1>
       <h4>File should be Jpeg, Png,...</h4>
-      <input></input>
+      <form>
+        <InputImg />
+      </form>
       <p>Or</p>
       <button>Choose a file</button>
     </div>
