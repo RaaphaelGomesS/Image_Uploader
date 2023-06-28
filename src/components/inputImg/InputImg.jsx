c0e5b413ef6da4900b420091cc312b203e18c15d
Raphael Gomes <rafagomes1706@gmail.com>
2023-06-28 16:54:46 -0300

    style: adiciona a imagem do input drag and drop

--- src/components/inputImg/InputImg.jsx
+++ src/components/inputImg/InputImg.jsx
@@ -1,9 +1,16 @@
 import "./inputImg.css";
 
 export default function InputImg(props) {
+
   return (
     <div className="imgInput">
       <input className="input" type="file" accept="image/png,jpeg" />
+      <img
+        width="100"
+        height="100"
+        src="https://img.icons8.com/ios-glyphs/100/BDBDBD/full-image.png"
+        alt="Imagem de paisagem"
+      />
       <span>Drag & Drop your image here</span>
     </div>
   );
